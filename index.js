985abc13ef558c18b1becf133f68c9bdc34a2b3e
jschmitt <jasonschmittcode@gmail.com>
2020-11-20 18:49:36 -0500

    basic routes setup for get post put and delete

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import routes from "./src/routes/crmRoutes";
 
 const app = express();
 const PORT = 8081;
+
+routes(app);
 
 app.get("/", (req, res) => {
   res.send(`hi on port ${PORT}`);
