6356060d887899cf3d4b4d7d8b919acf0bf3706b
jschmitt <jasonschmittcode@gmail.com>
2020-11-23 11:18:33 -0500

    finalizing secured endpoints

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -6,30 +6,42 @@ import {
   deleteContact,
 } from "../controllers/crmController";
 
+import { login, register, loginRequired } from "../controllers/userController";
+
 const routes = (app) => {
   app
     .route("/contact")
     // get all contacts
-    .get((req, res, next) => {
-      // middleware
-      console.log(`Request from: ${req.originalUrl}`);
-      console.log(`Request type: ${req.method}`);
-      next();
-    }, getContacts)
+    .get(
+      (req, res, next) => {
+        // middleware
+        console.log(`Request from: ${req.originalUrl}`);
+        console.log(`Request type: ${req.method}`);
+        next();
+      },
+      loginRequired,
+      getContacts
+    )
 
     // add a new contact
-    .post(addNewContact);
+    .post(loginRequired, addNewContact);
 
   app
     .route("/contact/:contactID")
     // get a specific contact
-    .get(getContactWithID)
+    .get(loginRequired, getContactWithID)
 
     // update a specific contact
-    .put(updateContact)
+    .put(loginRequired, updateContact)
 
     // delete a specific contact
-    .delete(deleteContact);
+    .delete(loginRequired, deleteContact);
+
+  // register new user route
+  app.route("/auth/register").post(register);
+
+  // login route
+  app.route("/login").post(login);
 };
 
 export default routes;
