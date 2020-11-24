b48d168baca4100030b229f91af439ccd3d7420a
jschmitt <jasonschmittcode@gmail.com>
2020-11-24 17:01:18 -0500

    cors testing on server

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ import bodyParser from "body-parser";
 import jsonwebtoken from "jsonwebtoken";
 
 const app = express();
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-type,Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
   next();
 });
 // enable cors
